a629fa005ca8f33de1ad751085b62b330b272c50
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2023-02-16 13:27:34 +0100

    Consolidate creation of divs and grid

--- main.js
+++ main.js
@@ -1,19 +1,16 @@
 const container = document.querySelector('.container');
-const squaresPerSide = 21;
+const squaresPerSide = 15;
 
-createDivs(squaresPerSide);
 createGrid(squaresPerSide);
 
-function createDivs(squaresPerSide) {
+function createGrid(squaresPerSide) {
+    // Create the divs, then put them in a grid.
     const numDivs = squaresPerSide ** 2;
     for (let i = 0; i < numDivs; i++) {
         const div = document.createElement('div');
         container.appendChild(div);
     }
-}
-
-function createGrid(squaresPerSide) {
-    /* The divs made by createDivs don't have an explicit shape yet.
+    /* The divs don't have an explicit shape yet.
     Here they're placed in an implicit grid such that they become square. */
     container.style.gridTemplateColumns = `repeat(${squaresPerSide}, 1fr)`
 }
