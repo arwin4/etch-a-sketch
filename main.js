2b56a1255e2cfcdbb497252aaa45f2f29b1322f0
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2023-02-16 12:44:28 +0100

    Implement flexible grid, non-hardcoded css

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 const container = document.querySelector('.container');
+const squaresPerSide = 21;
 
-for (let i = 0; i < (20 ** 2); i++) {
-    const square = document.createElement('div');
-    container.appendChild(square);
+createDivs(squaresPerSide);
+createGrid(squaresPerSide);
+
+function createDivs(squaresPerSide) {
+    const numDivs = squaresPerSide ** 2;
+    for (let i = 0; i < numDivs; i++) {
+        const div = document.createElement('div');
+        container.appendChild(div);
+    }
+}
+
+function createGrid(squaresPerSide) {
+    /* The divs made by createDivs don't have an explicit shape yet.
+    Here they're placed in an implicit grid such that they become square. */
+    container.style.gridTemplateColumns = `repeat(${squaresPerSide}, 1fr)`
 }
