23a2831d0fbbac90986b511ee31bfe4d4041919d
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2023-02-16 14:37:54 +0100

    Change js indentation to 2 spaces

--- main.js
+++ main.js
@@ -5,22 +5,22 @@ createGrid(squaresPerSide);
 changeSquareColor();
 
 function createGrid(squaresPerSide) {
-    // Create the divs, then put them in a grid.
-    const numDivs = squaresPerSide ** 2;
-    for (let i = 0; i < numDivs; i++) {
-        const div = document.createElement('div');
-        container.appendChild(div);
-    }
-    /* The divs don't have an explicit shape yet.
-    Here they're placed in an implicit grid such that they become square. */
-    container.style.gridTemplateColumns = `repeat(${squaresPerSide}, 1fr)`
+  // Create the divs, then put them in a grid.
+  const numDivs = squaresPerSide ** 2;
+  for (let i = 0; i < numDivs; i++) {
+    const div = document.createElement('div');
+    container.appendChild(div);
+  }
+  /* The divs don't have an explicit shape yet.
+  Here they're placed in an implicit grid such that they become square. */
+  container.style.gridTemplateColumns = `repeat(${squaresPerSide}, 1fr)`
 }
 
 function changeSquareColor() {
-    const squares = document.querySelectorAll('.container div');
-    squares.forEach((square) => {
-        square.addEventListener('mouseover', () => {
-            square.classList.add('colorChanged');
-        });
-    })
+  const squares = document.querySelectorAll('.container div');
+  squares.forEach((square) => {
+    square.addEventListener('mouseover', () => {
+      square.classList.add('colorChanged');
+    });
+  })
 }
