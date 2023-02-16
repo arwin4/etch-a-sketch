472fb5c8b400fd7e52d4e1a1925c0c852530528e
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2023-02-16 14:37:01 +0100

    Implement div painting

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const container = document.querySelector('.container');
 const squaresPerSide = 15;
 
 createGrid(squaresPerSide);
+changeSquareColor();
 
 function createGrid(squaresPerSide) {
     // Create the divs, then put them in a grid.
@@ -13,4 +14,13 @@ function createGrid(squaresPerSide) {
     /* The divs don't have an explicit shape yet.
     Here they're placed in an implicit grid such that they become square. */
     container.style.gridTemplateColumns = `repeat(${squaresPerSide}, 1fr)`
+}
+
+function changeSquareColor() {
+    const squares = document.querySelectorAll('.container div');
+    squares.forEach((square) => {
+        square.addEventListener('mouseover', () => {
+            square.classList.add('colorChanged');
+        });
+    })
 }
